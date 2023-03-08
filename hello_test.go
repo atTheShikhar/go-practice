b2a0e9d92e9b47fa90dcd537659e2f3f103a4e9c
Shikhar Singh <shikharsingh.original@gmail.com>
2023-03-09 00:21:07 +0530

    progress: Hello, world / subtests / helper test func

--- hello_test.go
+++ hello_test.go
@@ -6,18 +6,19 @@ func TestHello(t *testing.T) {
 	t.Run("Test 1: ", func(t *testing.T) {
 		got := Hello("Shikhar")
 		want := "Hello, Shikhar"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Test 2: ", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
