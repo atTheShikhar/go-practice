d4191de91cea250bd87d91f3e333c49992a905cc
Shikhar Singh <shikharsingh.original@gmail.com>
2023-03-10 00:30:30 +0530

    prog: iteration / modification

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(charater string) string {
+func Repeat(charater string, num int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < num; i++ {
 		repeated += charater
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	exp := "aaaaa"
+	repeated := Repeat("a", 7)
+	exp := "aaaaaaa"
 	if repeated != exp {
 		t.Errorf("expected %q got %q", exp, repeated)
 	}
@@ -12,6 +12,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 3)
 	}
 }
