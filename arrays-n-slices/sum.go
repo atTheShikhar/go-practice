f5c4afb86fec0c0522cca86e23423aea000a6ddf
Shikhar Singh <shikharsingh.original@gmail.com>
2023-03-17 00:31:17 +0530

    progress: arrays n slices/sum of variable slices/sum of tails of slices

--- arrays-n-slices/sum.go
+++ arrays-n-slices/sum.go
@@ -1,9 +1,34 @@
 package main
 
-func Sum(nums [5]int) int {
+func Sum(nums []int) int {
 	sum := 0
+
 	for _, num := range nums {
 		sum += num
 	}
+
 	return sum
+}
+
+func SumAll(numsToSum ...[]int) []int {
+	var resultSlice []int
+
+	for _, nums := range numsToSum {
+		resultSlice = append(resultSlice, Sum(nums))
+	}
+
+	return resultSlice
+}
+
+func SumAllTails(numsToSum ...[]int) []int {
+	var res []int
+	for _, nums := range numsToSum {
+		if len(nums) > 0 {
+			tail := nums[1:]
+			res = append(res, Sum(tail))
+		} else {
+			res = append(res, 0)
+		}
+	}
+	return res
 }
--- arrays-n-slices/sum_test.go
+++ arrays-n-slices/sum_test.go
@@ -1,13 +1,50 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	t.Run("Single Slice Sum", func(t *testing.T) {
+		numbers := []int{3, 4, 5, 6, 7}
+		got := Sum(numbers)
+		want := 25
 
-	if got != want {
-		t.Errorf("got %d expected %d, given %v", got, want, numbers)
+		if got != want {
+			t.Errorf("got %d expected %d", got, want)
+		}
+	})
+
+	t.Run("Multi Slice Sum", func(t *testing.T) {
+		slice1 := []int{1, 2, 3, 4, 5}
+		slice2 := []int{3, 4, 5, 6, 7}
+		got := SumAll(slice1, slice2)
+		want := []int{15, 25}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v expected %v", got, want)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSum := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v expected %v", got, want)
+		}
 	}
+
+	t.Run("sum of all tails of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSum(t, got, want)
+	})
+
+	t.Run("sum of all tails of slices, with empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+		checkSum(t, got, want)
+	})
 }
