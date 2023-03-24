21545d36621d7e5e1cd2fa0ac8be3ca9081c2dc5
Shikhar Singh <shikharsingh.original@gmail.com>
2023-03-24 20:21:18 +0530

    progress: mocking/ configurable sleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -17,6 +17,15 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := 3; i > 0; i-- {
 		fmt.Fprintln(out, i)
@@ -26,6 +35,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 type SpySleeper struct {
@@ -11,6 +13,30 @@ type SpySleeper struct {
 
 func (s *SpySleeper) Sleep() {
 	s.Calls++
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type SpyCountdown struct {
+	Calls []string
+}
+
+func (s *SpyCountdown) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdown) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 func TestCounter(t *testing.T) {
@@ -34,4 +60,30 @@ Go!`
 			t.Errorf("not enough calls to sleeper, want 3 but got %d", spySleeper.Calls)
 		}
 	})
+
+	t.Run("test counter write and sleep order", func(t *testing.T) {
+		spyCountdown := &SpyCountdown{}
+
+		Countdown(spyCountdown, spyCountdown)
+
+		want := []string{
+			write, sleep, write, sleep, write, sleep, write,
+		}
+
+		if !reflect.DeepEqual(want, spyCountdown.Calls) {
+			t.Errorf("wanted calls %v, got %v", want, spyCountdown.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
